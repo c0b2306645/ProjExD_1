bad8c2e4e9f2800e68de309b3411a775cc9f7b2b
Kobayashi Itsuki <c0b2306645@edu.teu.ac.jp>
2024-06-11 17:58:00 +0900

    演習終わり

--- flying_koukaton.py
+++ flying_koukaton.py
@@ -13,24 +13,39 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     bg_img2 = pg.transform.flip(bg_img,True, False)
-    x = 0
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (300,200)
     tmr = 0
+
+    x1 = 0
+    y1 = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x1 = 0
+        y1 = 0
+        x = tmr%3200
+        kk_rct.move_ip(-1,0)
+        screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
 
-        screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img2, [x+1600, 0])
-        screen.blit(bg_img, [x+3200, 0])
-        screen.blit(bg_img2, [x+4800, 0])
-        kk_rct = kk_img.get_rect()
-        kk_rct.center = 300,200
+        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            y1 -= 1
+        if key_lst[pg.K_DOWN]:
+            y1 +=1
+        if key_lst[pg.K_LEFT]:
+            x1 -=1
+        if key_lst[pg.K_RIGHT]:
+            x1 +=2
         screen.blit(kk_img, kk_rct)
+        kk_rct.move_ip(x1,y1)
         pg.display.update()
-        tmr += 1    
-        x -= 1    
-        if x == -3200:
-            x = 0
+        tmr += 1 
+        x += 1   
         clock.tick(200)
 
 
